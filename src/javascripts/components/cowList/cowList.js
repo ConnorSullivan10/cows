3b48d528d8b017aa4bf8ba283bd9f7a88e1a3868
Connor Sullivan <cksullvn@gmail.com>
2019-10-28 19:00:57 -0500

    cows are an array of objects

--- src/javascripts/components/cowList/cowList.js
+++ src/javascripts/components/cowList/cowList.js
@@ -1,11 +1,21 @@
-import cows from '../../helpers/data/cows';
+import cowData from '../../helpers/data/cows';
 
 import './cowList.scss';
 
 const buildCows = () => {
-  cows.getCows()
+  cowData.getCows()
     .then((response) => {
-      console.log('it worked!', response.data);
+      const demCows = response.data.cows;
+      const cows = [];
+      Object.keys(demCows).forEach((cowId) => {
+        // demCows['cow3']
+        // object.keys gets you an array of all the keys in an object
+        demCows[cowId].id = cowId;
+        // .id assigns an id and creates (= sign) a string
+        cows.push(demCows[cowId]);
+        // use bracket notation since cowID is a string
+      });
+      console.log('it worked!', cows);
     })
     .catch((error) => {
       console.error('shit broke', error);
--- src/javascripts/helpers/data/cows.js
+++ src/javascripts/helpers/data/cows.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
-const getCows = () => axios.get('https://teamtreehouse.com/ashleyclaiborne.json');
+// const getCows = () => axios.get('https://teamtreehouse.com/ashleyclaiborne.json');
+const getCows = () => axios.get('../../../../db/cows.json');
 
 export default { getCows };
