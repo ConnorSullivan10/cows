7891b6a7ef65b019bfe2287c08e229631d55e492
Connor Sullivan <cksullvn@gmail.com>
2019-10-28 19:14:25 -0500

    made getCows a promise

--- src/javascripts/components/cowList/cowList.js
+++ src/javascripts/components/cowList/cowList.js
@@ -5,17 +5,9 @@ import './cowList.scss';
 const buildCows = () => {
   cowData.getCows()
     .then((response) => {
-      const demCows = response.data.cows;
-      const cows = [];
-      Object.keys(demCows).forEach((cowId) => {
-        // demCows['cow3']
-        // object.keys gets you an array of all the keys in an object
-        demCows[cowId].id = cowId;
-        // .id assigns an id and creates (= sign) a string
-        cows.push(demCows[cowId]);
-        // use bracket notation since cowID is a string
-      });
-      console.log('it worked!', cows);
+      console.log('cow array from cowList', response);
+
+      // call a domStringBuilder functiona and pass in cows
     })
     .catch((error) => {
       console.error('shit broke', error);
--- src/javascripts/helpers/data/cows.js
+++ src/javascripts/helpers/data/cows.js
@@ -1,6 +1,25 @@
 import axios from 'axios';
 
 // const getCows = () => axios.get('https://teamtreehouse.com/ashleyclaiborne.json');
-const getCows = () => axios.get('../../../../db/cows.json');
+const getCows = () => new Promise((resolve, reject) => {
+  // promise in a promise
+  axios.get('../../../../db/cows.json').then((response) => {
+    const demCows = response.data.cows;
+    const cows = [];
+    Object.keys(demCows).forEach((cowId) => {
+      // demCows['cow3']
+      // object.keys gets you an array of all the keys in an object
+      demCows[cowId].id = cowId;
+      // .id assigns an id and creates (= sign) a string that's pushed into the array
+      cows.push(demCows[cowId]);
+      // use bracket notation since cowID is a string
+    });
+    resolve(cows);
+  }).catch((error) => reject(error));
+  // take result of error and pass it as the error message in the new promise
+});
+
+
+// axios.get('../../../../db/cows.json');
 
 export default { getCows };
